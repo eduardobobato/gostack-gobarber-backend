3f41e4590a0952674067f26234c93bd8d917484f
Eduardo <eduardobobato@hotmailcom.br>
2020-12-16 01:56:32 -0300

    Refactory geraçãode hash e testes unitários

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,8 +1,9 @@
-import User from '@modules/users/infra/typeorm/entities/User';
 import { injectable, inject } from 'tsyringe';
-import { hash } from 'bcryptjs';
+
+import User from '@modules/users/infra/typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
   name: string;
@@ -14,7 +15,9 @@ interface IRequest {
 class CreateUserService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
     ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -23,7 +26,7 @@ class CreateUserService {
     if (checkUserExists)
       throw new AppError('Email address already used.');
 
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await this.hashProvider.generateHash(password);
 
     const user = await this.usersRepository.create({
       name,
